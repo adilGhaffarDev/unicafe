8e91a6b85b9cea652c2a462a985ff1a6d2933c0c
adil ghaffar <adilghaffar64@gmail.com>
2020-08-03 04:39:53 +0300

    part 1.6 to 1.11 are done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## unicafe
+fullstackopen
--- src/index.js
+++ src/index.js
@@ -1,15 +1,73 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({onClick,text}) => <button onClick={onClick}>{text}</button>
+const Header = ({header}) => {
+  return (
+    <>
+      <h1>{header}</h1>
+    </>
+  )
+}
+const Statistics = ({good,neutral,bad}) => {
+  const all = good+neutral+bad
+  if(all === 0){
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+  <table>
+    <Statistic text="good" value ={good} />
+    <Statistic text="neutral" value ={neutral} />
+    <Statistic text="bad" value ={bad} />
+    <Statistic text="all" value ={all} />
+    <Statistic text="average" value ={((good/all)*1)+((bad/all)*-1)} />
+    <Statistic text="positive" value = {(good/all)*100 + "%"} />
+    </table>)
+}
+
+const Statistic = ({text,value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const headerFeedback = 'give feedback'
+  const headerStatitics = 'statistics'
+  let all = good+neutral+bad
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <Header header={headerFeedback}/>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <Header header={headerStatitics}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
